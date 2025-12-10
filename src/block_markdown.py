b15b676a903e5077ea9ad8f76dcf2d1194db482c
nrjones-dev <nrjones.dev@gmail.com>
2025-12-10 17:04:46 +0000

    updates block markdown functionality

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,50 @@
+import re
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     md_blocks = markdown.split("\n\n")
     filtered_blocks = list(filter(None, map(str.strip, md_blocks)))
-
     return filtered_blocks
+
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if re.match(r"^#{1,6}\s", block):
+        return BlockType.HEADING
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    if all_lines_start_with(lines, ">"):
+        return BlockType.QUOTE
+    if all_lines_start_with(lines, "- "):
+        return BlockType.ULIST
+
+    if is_ordered_list(lines):
+        return BlockType.OLIST
+
+    return BlockType.PARAGRAPH
+
+
+def is_ordered_list(lines):
+    for i, line in enumerate(lines):
+        expected_prefix = f"{i + 1}. "
+        if not line.startswith(expected_prefix):
+            return False
+    return True
+
+
+def all_lines_start_with(lines, prefix):
+    for line in lines:
+        if not line.startswith(prefix):
+            return False
+    return True
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import BlockType, all_lines_start_with, block_to_block_type, is_ordered_list, markdown_to_blocks
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -44,3 +44,40 @@ this is the 6th line
                 "this is the 5th line\nthis is the 6th line",
             ],
         )
+
+    def test_block_to_block_type_general(self):
+        cases = [
+            ("# This is a heading", BlockType.HEADING),
+            ("###### This is a heading", BlockType.HEADING),
+            ("```\nThis is a\ncode block\n```", BlockType.CODE),
+            ("```This is a code block```", BlockType.CODE),
+            (">This is a quoted\n>block of\n>text", BlockType.QUOTE),
+            ("- This is an unordered list\n- with items\n- in it", BlockType.ULIST),
+            ("1. This is an ordered list\n2. with items\n3. in it", BlockType.OLIST),
+            ("This is a normal paragraph\nHow do you do?", BlockType.PARAGRAPH),
+        ]
+        for block, expected in cases:
+            with self.subTest(block):
+                self.assertEqual(expected, block_to_block_type(block))
+
+    def test_is_ordered_list(self):
+        block = "1. foo\n2. bar\n3. sushi\n4. eggs"
+        lines = block.split("\n")
+        self.assertTrue(is_ordered_list(lines))
+
+    def test_is_ordered_list_false(self):
+        block = "1. foo\n3. bar"
+        lines = block.split("\n")
+        self.assertFalse(is_ordered_list(lines))
+
+    def test_all_lines_start_with_quote(self):
+        block = ">foo\n>bar\n>sushi\n>eggs"
+        lines = block.split("\n")
+        prefix = ">"
+        self.assertTrue(all_lines_start_with(lines, prefix))
+
+    def test_all_lines_start_with_ulist(self):
+        block = "- foo\n- bar\n- sushi\n- eggs"
+        lines = block.split("\n")
+        prefix = "- "
+        self.assertTrue(all_lines_start_with(lines, prefix))
