a96e07edbe6d0f7ddef70178ab628f0e5b55d709
Nathan Jones <115369332+nrjones-dev@users.noreply.github.com>
2026-01-16 23:27:00 +0000

    add temp update

--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,14 @@
 import unittest
 
-from block_markdown import BlockType, all_lines_start_with, block_to_block_type, is_ordered_list, markdown_to_blocks
+from block_markdown import (
+    BlockType,
+    all_lines_start_with,
+    block_to_block_type,
+    block_to_html_node,
+    is_ordered_list,
+    markdown_to_blocks,
+    markdown_to_html_node,
+)
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -81,3 +89,40 @@ this is the 6th line
         lines = block.split("\n")
         prefix = "- "
         self.assertTrue(all_lines_start_with(lines, prefix))
+
+    def test_block_to_html_node_paragraph(self):
+        block = "This is a normal paragraph\nHow do you do?"
+        node = block_to_html_node(block)
+        self.assertTrue(node.tag, "<p>")
+
+    def test_paragraphs(self):
+        md = """
+    This is **bolded** paragraph
+    text in a p
+    tag here
+
+    This is another paragraph with _italic_ text and `code` here
+
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+    ```
+    This is text that _should_ remain
+    the **same** even with inline stuff
+    ```
+    """
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
