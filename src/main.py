0647982cb9628ee83e212009d653d3d71430b870
nrjones-dev <nrjones.dev@gmail.com>
2026-04-30 13:48:18 +0100

    updates copying function

--- src/main.py
+++ src/main.py
@@ -1,35 +1,38 @@
+import logging
 import os
 import shutil
 
-from src.html_node import HTMLNode, LeafNode
-from src.text_node import TextNode, TextType
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+logger.setLevel(logging.INFO)
 
-PUBLIC_PATH = "public"
+PUBLIC_PATH = "./public"
+STATIC_PATH = "./static"
 
 
 def main():
-    new_text_node = TextNode("This is a test", TextType.TEXT, "https://github.com")
-    new_html_node = HTMLNode("p", "This is a paragraph")
-    new_leaf_html_node = LeafNode("a", "click this link", {"href": "https://www.google.com"})
-    print(new_text_node)
-    print(new_html_node)
-    print(new_leaf_html_node)
-
-
-def clr_dir():
+    logger.info("Clearing public directory")
     if os.path.exists(PUBLIC_PATH):
-        for item in os.listdir(PUBLIC_PATH):
-            path = os.path.join(PUBLIC_PATH, item)
-            if os.path.isdir(path):
-                shutil.rmtree(path)
-            else:
-                os.remove(path)
+        shutil.rmtree(PUBLIC_PATH)
+
+    logger.info("Copying Static files and directories to Public.")
+    copy_source_files(STATIC_PATH, PUBLIC_PATH)
 
 
-def copy_content():
-    pass
+def copy_source_files(source_dir_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    logger.info(f"Entering Directory: {source_dir_path}")
+    for item in os.listdir(source_dir_path):
+        source_path = os.path.join(source_dir_path, item)
+        dest_path = os.path.join(dest_dir_path, item)
+        logger.info(f"Copying: {source_path} -> {dest_path}")
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, dest_path)
+        else:
+            copy_source_files(source_path, dest_path)
 
 
 if __name__ == "__main__":
     main()
-    clr_dir()
